98815f29cc72d487eead08d6877e245f90f6fdb0
YogeshMeghnani <yogeshmeghnani123@gmail.com>
2020-05-24 23:59:47 +0530

    studentInfo:second

--- src/main/webapp/JS/studentInfo.js
+++ src/main/webapp/JS/studentInfo.js
@@ -1,15 +1,11 @@
- // var id = JSON.parse(sessionStorage.getItem("id"));
- // var name = JSON.parse(sessionStorage.getItem("name"));
- // var rollno = JSON.parse(sessionStorage.getItem("rollno"));
- // var course = JSON.parse(sessionStorage.getItem("course"));
- // var year = JSON.parse(sessionStorage.getItem("year"));
+
+
 function StudentInfo()
 {
-    var id = JSON.parse(sessionStorage.getItem("id"));
-    var name = JSON.parse(sessionStorage.getItem("name"));
-    var rollno = JSON.parse(sessionStorage.getItem("rollno"));
-    var course = JSON.parse(sessionStorage.getItem("course"));
-    var year = JSON.parse(sessionStorage.getItem("year"));
+    var name = sessionStorage.getItem("name");
+    var rollno = sessionStorage.getItem("rollno");
+    var course = sessionStorage.getItem("course");
+    var year = sessionStorage.getItem("year");
   document.getElementById("studentInfo").children[0].innerHTML += "<li>"+name+"</li>";
   document.getElementById("studentInfo").children[0].innerHTML += "<li>"+rollno+"</li>";
   document.getElementById("studentInfo").children[0].innerHTML += "<li>"+course+"</li>";
