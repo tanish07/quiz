c3ef75c949b2b7eb11b0476c3518ed6322e53ba4
YogeshMeghnani <yogeshmeghnani123@gmail.com>
2020-05-24 23:01:20 +0530

    studentTable:second

--- src/main/webapp/JS/studentTable.js
+++ src/main/webapp/JS/studentTable.js
@@ -1,7 +1,7 @@
  var id = JSON.parse(sessionStorage.getItem("id"));
  if(id != null) {
      var cu_id = "id=" + id;
-     var api="http://localhost:8080/Student3_war/webapi/Student/getTimeTable?"+cu_id;
+     var api="http://localhost:8080/quiz_war/webapi/quiz/testlistbyuserid?"+cu_id;
      $.get(api, function (student, status) {
          console.log(student);
          if (status == "success") {
@@ -20,12 +20,12 @@
              "<th>Marks</th></tr> </thead>";
              for (var i = 0; i < student.length; i++) 
              {
-                 var q1="http://localhost:8080/Student3_war/HTML/FacultyInfo.html?testid="+student[i][0]+">";
-                 var q2="http://localhost:8080/Student3_war/HTML/FacultyInfo.html?testaid="+student[i][4]+">";
+                 var q1="http://localhost:8080/Student3_war/HTML/FacultyInfo.html?testid="+student[i][0]+"?testaid="+student[i][4]+">";
+                 // var q2="http://localhost:8080/Student3_war/HTML/FacultyInfo.html?testaid="+student[i][4]+">";
                 
                  student_tests_list +=  '<tr><td><a href='+q1+student[i][1] + '</a></td>'
-                     + '<td>'+q2+student[i][2]+'</td>'
-                     + '<td>'+q3+student[i][3]+ '</td></tr>';
+                     + '<td>'+student[i][2]+'</td>'
+                     + '<td>'+student[i][3]+ '</td></tr>';
              }
              $('#QuizTable').append(student_tests_list);
          }
@@ -34,6 +34,6 @@
  }
  else
  {
-     location.replace("http://localhost:8080/Student3_war/HTML/Login.html");
+     location.replace("http://localhost:8080/quiz_war/HTML/Login.html");
  }
 
