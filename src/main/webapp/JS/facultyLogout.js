ab0fd9e10d4dcb99551b64ca0e6346b40274bca5
Aditya-tiwari17 <adityatiwari2117@gmail.com>
2020-05-30 15:06:31 +0530

    Add files via upload

---
+++ src/main/webapp/JS/facultyLogout.js
@@ -0,0 +1,22 @@
+function logout()
+{
+	var id = sessionStorage.getItem("id");
+    var api = "/quiz_war/webapi/quiz/facultylogout";
+    var text =  {"id":id};
+
+    $.ajax
+    ({
+        type : "POST",
+        url : api,
+        data : text,
+        async: false,
+        cache: false,
+        statusCode:
+        {
+            200:    function(response)
+            {
+                location.replace("http://localhost:8080/quiz_war/HTML/facultyLogin.html");
+            }
+        }
+    });
+}
